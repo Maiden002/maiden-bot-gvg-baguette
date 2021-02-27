16301222c9b984d225f9e46319573a35a4df7d59
Maiden002 <79310019+Maiden002@users.noreply.github.com>
2021-02-27 13:31:59 +0100

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,6 @@ module.exports = class help {
     }
 
     static action (message) {
-        message.channel.send('\n**Menu Aide - Maiden Bot (GVG Baguette)**\n\n| **md.r** (tagDiscord optionel) : Vous enregistre auprès du bot. Pour enregistrer un autre joueur, ajouter son tag après la commande.\n\n| **md.ur** (tagDiscord optionel) : Vous supprimer auprès du bot. Pour supprimer un autre joueur, ajouter son tag après la commande.\n\n| **md.tw_assignation** : Envoi par MP les assignations de pose en défense pour les joueurs participant à la TW.\n');
+        message.channel.send('\n**Menu Aide - Maiden Bot (GVG Baguette)**\n\n| **md.r** (tagDiscord optionel) : Vous enregistre auprès du bot. Pour enregistrer un autre joueur, ajouter son tag après la commande.\n\n| **md.ur** (tagDiscord optionel) : Vous supprimer auprès du bot. Pour supprimer un autre joueur, ajouter son tag après la commande.\n\n| **md.tw_assignation** : Envoi par MP les assignations de pose en défense pour les joueurs participant à la TW.\n\n| **md.lp ** : Liste PLayer - Renvoie la liste des joueurs inscrits auprès du bot pour la guilde.\n');
     }
 }
