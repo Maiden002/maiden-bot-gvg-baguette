5bc9c008dd715e9959c1bfca2f445b5313b04bed
Maiden002 <79310019+Maiden002@users.noreply.github.com>
2021-06-28 14:59:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -427,47 +427,47 @@ async function planDeFarmMember(message, user) {
                         }
 
                         if(pdf2 != 'NA'){
-                            message = " - " + message + pdf2 + "\n"
+                            message = message + " - " + pdf2 + "\n"
                         }
 
                         if(pdf3 != 'NA'){
-                            message = " - " + message + pdf3 + "\n"
+                            message = message + " - " + pdf3 + "\n"
                         }
 
                         if(pdf4 != 'NA'){
-                            message = " - " + message + pdf4 + "\n"
+                            message = message + " - " + pdf4 + "\n"
                         }
 
                         if(pdf5 != 'NA'){
-                            message = " - " + message + pdf5 + "\n"
+                            message = message + " - " + pdf5 + "\n"
                         }
 
                         if(pdf6 != 'NA'){
-                            message = " - " + message + pdf6 + "\n"
+                            message = message + " - " + pdf6 + "\n"
                         }
 
                         if(pdf7 != 'NA'){
-                            message = " - " + message + pdf7 + "\n"
+                            message = message + " - " + pdf7 + "\n"
                         }
 
                         if(pdf8 != 'NA'){
-                            message = " - " + message + pdf8 + "\n"
+                            message = message + " - " + pdf8 + "\n"
                         }
 
                         if(pdf9 != 'NA'){
-                            message = " - " + message + pdf9 + "\n"
+                            message = message + " - " + pdf9 + "\n"
                         }
 
                         if(pdf10 != 'NA'){
-                            message = " - " + message + pdf10 + "\n"
+                            message = message + " - " + pdf10 + "\n"
                         }
 
                         if(pdf11 != 'NA'){
-                            message = " - " + message + pdf11 + "\n"
+                            message = message + " - " + pdf11 + "\n"
                         }
 
                         if(pdf12 != 'NA'){
-                            message = " - " + message + pdf12 + "\n"
+                            message = message + " - " + pdf12 + "\n"
                         }
                         
                         if(message != ''){
